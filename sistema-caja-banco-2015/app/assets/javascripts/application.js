f76ef761b40cc349dfb801dd3b25362198a39ae8
Emilio Centurion <emicenturion13@gmail.com>
2015-10-01 02:40:31 -0400

    Revert "fusionar rama fatimaTalavera-master"
    This reverts commit 95306522eb37de7f7375cdddbe76b7e4cd479116.

--- sistema-caja-banco-2015/app/assets/javascripts/application.js
+++ sistema-caja-banco-2015/app/assets/javascripts/application.js
@@ -12,60 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require turbolinks
-//= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require bootstrap-sprockets
 //= require_tree .
-
-$(document).ready(function() {
-	$("#btn1").click(function(){
-		$('#form-toggle').fadeToggle();
-		if($('#gbtn1').hasClass('glyphicon glyphicon-plus')){
-			$('#gbtn1').removeClass('glyphicon glyphicon-plus');
-			$('#gbtn1').addClass('glyphicon glyphicon-chevron-up');
-		}else{
-			$('#gbtn1').removeClass('glyphicon glyphicon-chevron-up');
-			$('#gbtn1').addClass('glyphicon glyphicon-plus');
-		}
-	});
-	$('#table').DataTable({
-	  // ajax: ...,
-	  // autoWidth: false,
-	  // pagingType: 'full_numbers',
-	  // processing: true,
-	  // serverSide: true,
-
-	  // Optional, if you want full pagination controls.
-	  // Check dataTables documentation to learn more about available options.
-	  // http://datatables.net/reference/option/pagingType
-		 "language": {
-	        "sProcessing":    "Procesando...",
-	        "sLengthMenu":    "Mostrar _MENU_ registros",
-	        "sZeroRecords":   "No se encontraron resultados",
-	        "sEmptyTable":    "Ningún dato disponible en esta tabla",
-	        "sInfo":          "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-	        "sInfoEmpty":     "Mostrando registros del 0 al 0 de un total de 0 registros",
-	        "sInfoFiltered":  "(filtrado de un total de _MAX_ registros)",
-	        "sInfoPostFix":   "",
-	        "sSearch":        "Buscar:",
-	        "sUrl":           "",
-	        "sInfoThousands":  ",",
-	        "sLoadingRecords": "Cargando...",
-	        "oPaginate": {
-	            "sFirst":    "Primero",
-	            "sLast":    "Último",
-	            "sNext":    "Siguiente",
-	            "sPrevious": "Anterior"
-	        },
-	        "oAria": {
-	            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-	            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-			}
-	    }
-	});
-
-	$('input[data-role=money]').autoNumeric('init');
-});
-
-
