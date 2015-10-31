ccc359749b3fa49f1612d11a04e200df2a8b5b9c
ergo <stratofor@gmail.com>
2015-10-31 15:30:27 -0300

    Se Agrego un modal funcional de paises en el ABM Bancos

--- sistema-caja-banco-2015/app/controllers/bancos_controller.rb
+++ sistema-caja-banco-2015/app/controllers/bancos_controller.rb
@@ -6,6 +6,7 @@ class BancosController < ApplicationController
   def index
     @banco = Banco.new
     @bancos = Banco.all
+    @pais = Pai.all
     @pai = Pai.new
     @ciudad = Ciudad.new
   end
@@ -25,6 +26,7 @@ class BancosController < ApplicationController
     @bancos = Banco.all
     @pai = Pai.find(params[:id])
     @ciudad = Ciudad.find(params[:id])
+    @pais = Pai.all
   end
 
   # POST /bancos
@@ -37,7 +39,7 @@ class BancosController < ApplicationController
         format.html { redirect_to bancos_url, notice: 'Creaste un nuevo banco correctamente.' }
         format.json { render :show, status: :created, location: @banco }
       else
-        format.html { render :new }
+        format.html { render :new}
         format.json { render json: @banco.errors, status: :unprocessable_entity }
       end
     end
