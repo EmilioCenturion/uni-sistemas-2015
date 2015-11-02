1278f3062e459fe0664fdab9f90d8c759e3d81e8
ergo <stratofor@gmail.com>
2015-11-02 00:13:49 -0300

    Se Mejoro la interfaz del ABM para Movimiento de Bancos y ya es funcional

--- sistema-caja-banco-2015/app/models/motivo_movimiento_banco.rb
+++ sistema-caja-banco-2015/app/models/motivo_movimiento_banco.rb
@@ -1,2 +1,3 @@
 class MotivoMovimientoBanco < ActiveRecord::Base
+	has_many :movimiento_bancos
 end
--- sistema-caja-banco-2015/app/models/movimiento_banco.rb
+++ sistema-caja-banco-2015/app/models/movimiento_banco.rb
@@ -1,4 +1,21 @@
 class MovimientoBanco < ActiveRecord::Base
-  belongs_to :cuenta
+  belongs_to :cuentum
   belongs_to :motivo_movimiento_banco
+
+
+
+
+  before_create :create_movimiento
+
+  protected
+  def create_movimiento
+  	self.fecha = Time.now
+  	saldo = Cuentum.find(self.cuenta_id).saldo
+  	if self.es_ingreso
+  		saldo= saldo + self.monto_efectivo + self.monto_cheque
+  	else
+  		saldo= saldo - self.monto_efectivo - self.monto_cheque
+  	end
+  	Cuentum.update(self.cuenta_id, :saldo => saldo)
+  end
 end
