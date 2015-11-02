5faef2757974ca514a84c9b611e1d505f4217f8e
ergo <stratofor@gmail.com>
2015-11-02 15:40:29 -0300

    Se Agrego las validaciones para el ABM Movimientos Bancos

--- sistema-caja-banco-2015/app/models/movimiento_banco.rb
+++ sistema-caja-banco-2015/app/models/movimiento_banco.rb
@@ -3,6 +3,22 @@ class MovimientoBanco < ActiveRecord::Base
   belongs_to :motivo_movimiento_banco
 
 
+  validates :cuenta_id, :presence => {:message => "No puede estar en blanco"}
+
+  validates :monto_efectivo,  :presence => {:message => "No puede estar en blanco"},
+                              :length => {minimum: 1, :message => "Debe tener como minimo 6 caracteres"},
+                              :length => {maximum: 30, :message => "Debe tener como maximo 20 caracteres"},
+                              :numericality => {:only_double => true, :message => "Debe ser un numero"}
+
+  validates :monto_cheque, :presence => {:message => "No puede estar en blanco"},
+                            :length => {minimum: 1, :message => "Debe tener como minimo 6 caracteres"},
+                            :length => {maximum: 30, :message => "Debe tener como maximo 20 caracteres"},
+                            :numericality => {:only_double => true, :message => "Debe ser un numero"}
+
+  validates :descripcion, :presence => {:message => "No puede estar en blanco"},
+                          :length => {minimum: 8, :message => "Debe tener como minimo 6 caracteres"},
+                          :length => {maximum: 50, :message => "Debe tener como maximo 20 caracteres"},
+                          :format => {with: /[a-zA-ZÃ?ï¿½Ã?Â±Ã?Â¡Ã?Â©Ã?Â­Ã?Â³Ã?Âº]/, message: 'Solo permite letras'}
 
 
   before_create :create_movimiento
