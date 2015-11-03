d728f77993f00fa767166fa1d4226ec1aee0ca6e
fatima Talavera <fa.talavera95@gmail.com>
2015-11-03 16:49:40 -0300

    Vista de boleta de deposito

--- sistema-caja-banco-2015/app/models/boleta_deposito.rb
+++ sistema-caja-banco-2015/app/models/boleta_deposito.rb
@@ -2,4 +2,5 @@ class BoletaDeposito < ActiveRecord::Base
   belongs_to :cuentum
   belongs_to :apertura_caja
   has_many :boleta_deposito_detalles
+  accepts_nested_attributes_for :boleta_deposito_detalles, allow_destroy: true
 end
