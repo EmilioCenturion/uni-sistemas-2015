a217823e250efb86b5c4d4c17cbf2a58dce7b7fd
ergo <stratofor@gmail.com>
2015-11-22 15:18:21 -0300

    Configuracion de la Gema CanCan para roles de usuario

--- sistema-caja-banco-2015/app/controllers/cuenta_controller.rb
+++ sistema-caja-banco-2015/app/controllers/cuenta_controller.rb
@@ -1,4 +1,5 @@
 class CuentaController < ApplicationController
+  load_and_authorize_resource
   before_action :set_cuentum, only: [:show, :edit, :update, :destroy]
 
   # GET /cuenta
@@ -16,7 +17,7 @@ class CuentaController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        pdf = CuentumPdf.new(@cuentum, view_context)
+        pdf = CuentumPdf.new([@cuentum, current_user.username], view_context)
         send_data pdf.render, filename: "libro_banco_#{@cuentum.nro_cuenta}.pdf",
                               type: "application/pdf",
                               disposition: "inline"
--- sistema-caja-banco-2015/app/models/user.rb
+++ sistema-caja-banco-2015/app/models/user.rb
@@ -1,7 +1,25 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :apertura_cajas
-  devise :database_authenticatable, :registerable,
+    audited
+  	has_many :apertura_cajas
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    ROLES = %i[administrador crear_banco]
+
+  	def roles=(roles)
+    	roles = [*roles].map { |r| r.to_sym }
+    	self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  	end
+
+  	def roles
+    	ROLES.reject do |r|
+      	((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
+    	end
+  	end
+
+  	def has_role?(role)
+    	roles.include?(role)
+  	end
 end
