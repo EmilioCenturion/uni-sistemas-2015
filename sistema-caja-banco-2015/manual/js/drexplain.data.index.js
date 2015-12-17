c93042cc2bd909173a4cc897834b5eab054a33b8
Emilio Centurion <emicenturion13@gmail.com>
2015-12-17 11:54:26 -0300

    Modelo de datos actualizado y carpeta con manual en formato pdf y html

---
+++ sistema-caja-banco-2015/manual/js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [1,1,2,3,4,5,6,12,13,7,14,15,16,17,8,9,10,11,18,19],
+	DREX_NODE_KEYWORDS_START: [0,1,1,5,5,20,20,20,20,20,20,20,20,20,20,20,20,20,20], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [1,1,5,5,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<NUEVA PALABRA CLAVE>","Manual de Usuario","1. Botones Principales","2. Ingreso al Sistema","3. Menu Bar","3.1 Nombre de la empresa y boton Home","3.2 Caja","3.3 Banco","3.4 Perfiles","3.5 Configuraciones","3.6 Auditoria","3.7 Usuario","3.2.1 Cajas","3.2.2 Aperturas","3.3.1 Bancos","3.3.2 Cuentas","3.3.3 Movimientos de Banco","3.3.4 Libro Banco","3.7.1 Editar Perfil","3.7.2 Cerrar Sesión"],
+	DREX_KEYWORD_CHILD_START: [1,2,5,5,5,12,12,14,18,18,18,18,20,20,20,20,20,20,20,20],
+	DREX_KEYWORD_CHILD_END: [2,5,5,5,12,12,14,18,18,18,18,20,20,20,20,20,20,20,20,20]
+	
+};
